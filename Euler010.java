dd0347c2139d5e30fd7542ee5862d0095caa021f
ycrep1993 <ycrep1993@gmail.com>
2015-09-09 11:11:16 +0200

    JAAAAAAAAAAAAAAA Opgelost

--- Euler010.java
+++ Euler010.java
@@ -1,30 +1,49 @@
+import java.util.Arrays;
+
 /**
- * Created by Percy on 7-9-2015.
+ * Created by Percy on 8-9-2015.
  */
+
 public class Euler010 {
     public static void main(String[] args){
         new Euler010().run();
     }
 
     public void run(){
+        boolean[] arr = new boolean[2000000];
+
+        Arrays.fill(arr, true);
+
+        arr[0] = false;
+        arr[1] = false;
+
         long antwoord = 0;
-        for(int i = 3; i < 2000000; i+=2){
-            System.out.println("run");
-            if(isPrime(i)){
-                antwoord = antwoord+i;
-                //System.out.print(i+",");
+
+        for(int i = 3; i < 2000000; i++){
+            //System.out.println("loopje 1 " + i);
+            if(arr[i]){
+                arr[i] = isPrime(i);
+                for(int n = 2; n*i < 2000000; n++){
+                    //System.out.println(n + "" + i);
+                    arr[n*i] = false;
+                }
             }
         }
-        antwoord = antwoord+2;
+
+        for(int i = 0; i < 2000000; i++){
+            if(arr[i]){
+                antwoord = antwoord + i;
+            }
+        }
+
         System.out.println(antwoord);
     }
 
     public boolean isPrime(int number){
-       for(int i = 3; i < number; i+= 2){
-            //System.out.println("isprime");
-            if(number % i == 0){
-                return false;
-            }
+        if(number==2) return true;
+        if(number%2==0) return false;
+        for(int i = 3; i*i <= number; i+=2){
+            if(number % i == 0) return false;
         }
         return true;
     }
